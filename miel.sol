330824a0277ef70af2d2c316f3fcb93b4ab69d5a
Moibe <moi_estrello@hotmail.com>
2022-05-14 21:35:41 -0500

    Create miel.sol

---
+++ miel.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import './yields-utils-v2/token/ERC20.sol';
+
+contract Miel is ERC20 {
+  constructor() 
+  ERC20('Miel', 'MIEL', 18) {
+    _mint(msg.sender, 1000);
+  }
+}
