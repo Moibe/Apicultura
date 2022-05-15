bbe8615f19384d7d5ed5200c91f8d8f1f3a1e73a
Moibe <moi_estrello@hotmail.com>
2022-05-14 21:38:48 -0500

    Más miel
    Se cambio de 1000 a 1,000,000 la cantidad producible de miel. Para que el ratio no sea 1 abeja = 1 miel.

--- miel.sol
+++ miel.sol
@@ -5,6 +5,6 @@ import './yields-utils-v2/token/ERC20.sol';
 contract Miel is ERC20 {
   constructor() 
   ERC20('Miel', 'MIEL', 18) {
-    _mint(msg.sender, 1000);
+    _mint(msg.sender, 1000000);
   }
 }
