5364344e62d3d177c59707a661cf64805f0a2ccc
Moibe <moi_estrello@hotmail.com>
2022-05-14 21:37:53 -0500

    Create abeja.sol

---
+++ abeja.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import './yields-utils-v2/token/ERC20Rewards.sol';
+import './yields-utils-v2/token/ERC20.sol';
+
+contract Abeja is ERC20Rewards {
+  constructor(address rewardsToken) 
+  ERC20Rewards('Abeja', 'BEE', 18, IERC20(rewardsToken)) {
+    _mint(msg.sender, 1000);
+  }
+}
